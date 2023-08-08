e00ee16e823339d43ce38b24e8afc8778af8ee56
Irakli Gogicha <108423675+gogicha007@users.noreply.github.com>
2023-08-08 21:34:34 +0400

    feat: add towel-sort code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix = []) {
+  return matrix
+      .reduce((arr, value, index) => {
+          if (index % 2 === 1) {
+              console.log(`odd: ${index} : ${value}`)
+              return [...arr, value.sort((a, b) => b - a)]
+          } else {
+              console.log(`even: ${index} :${value}`)
+              return [...arr, value]
+          }
+      }, [])
+      .flat()
 }
+
